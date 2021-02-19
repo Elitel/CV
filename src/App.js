dfcaccf2bb0be2296189a90d442a210c099bf77a
Elitel <Alitali@yandex.ru>
2021-02-19 08:11:00 +0300

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Bar from './components/Bar/Bar';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ссылка
-        </a>
-        <Bar color = "#121212" backgroundColor = "red" percent = {80}/>
-        <Bar color = "#345678" backgroundColor = "white" percent = {100}/>
+      <header>
+        <Navigation/>
       </header>
     </div>
   );
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = props => {
+  return (
+    <div className="nav">
+      <div className="navbar">
+        <ul>
+          <li className="nav-item">EN</li>
+          <li className="nav-item">1</li>
+          <li className="nav-item">2</li>
+          <li className="nav-item">3</li>
+          <li className="nav-item">4</li>
+          <li className="nav-item">5</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;

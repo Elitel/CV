c74a0d2bd66b318172b5de09bbe9c8ed82d2aa4c
Elitel <Alitali@yandex.ru>
2021-02-27 14:14:02 +0300

    chrome line

--- src/index.js
+++ src/index.js
@@ -12,6 +12,18 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

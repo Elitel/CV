b5bc5e4c61c7ff38e029a23a22c18309e0d5436d
Elitel <Alitali@yandex.ru>
2021-04-07 22:02:06 +0300

    Content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,28 @@
 import React from 'react';
 
-import {AnimatePresence, motion} from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Navigation from '../components/Navigation/Navigation';
 
 import { withNamespaces } from 'react-i18next';
 import styles from './Contact.module.css';
 
-function Contact({t}) {
+function Contact({ t }) {
     return (
         <div>
             <Navigation />
-            <div className= {styles.info}>
-                <div className= {styles.title}>
+            <div className={styles.info}>
+                <div className={styles.title}>
                     <p>&#60;h3&#62; </p>
                     <h1>{t('Contact')}</h1>
                     <p> &#60;/h3&#62;</p>
                 </div>
-                <div class="about">
-                  
-                </div>
+                <form>
+                    <label>
+                        Имя:
+    <input type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Отправить" />
+                </form>
             </div>
         </div>
     );
